50e474fd7706a8f5869ae89c40e89cace997f709
yellowclover <gpmaitl@gmail.com>
2016-02-21 00:08:26 -0800

    adding all initial code and got the home layout completed!

---
+++ App/mobile/src/main/java/com/patrickohalloran/represent/MainViewMobileActivity.java
@@ -0,0 +1,13 @@
+package com.patrickohalloran.represent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainViewMobileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view_mobile);
+    }
+}
