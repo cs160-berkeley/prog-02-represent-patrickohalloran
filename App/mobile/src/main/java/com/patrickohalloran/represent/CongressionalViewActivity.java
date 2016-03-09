910d7e1cb4ab1c24368f9f26bbdb972518900a26
yellowclover <gpmaitl@gmail.com>
2016-03-09 02:01:02 -0800

    got the sunlight locations to show up from within the congressional view class

--- App/mobile/src/main/java/com/patrickohalloran/represent/CongressionalViewActivity.java
+++ App/mobile/src/main/java/com/patrickohalloran/represent/CongressionalViewActivity.java
@@ -1,6 +1,10 @@
 package com.patrickohalloran.represent;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -12,6 +16,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -49,13 +54,29 @@ public class CongressionalViewActivity extends AppCompatActivity {
      */
     private ViewPager mViewPager;
 
+    private static final String DEBUG_TAG = "HttpExample";
+    private String zipCode;
+    private String lat;
+    private String lon;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congressional_view);
 
-        Intent httpIntent = new Intent(this, HTTPActivity.class);
-        startActivity(httpIntent);
+//        Intent httpIntent = new Intent(this, HTTPActivity.class);
+//        startActivity(httpIntent);
+        Intent locationIntent = getIntent();
+        if (locationIntent.hasExtra(getResources().getString(R.string.zip_code))) {
+            this.zipCode = locationIntent.getStringExtra(getResources().getString(R.string.zip_code));
+            myClickHandler(true);
+        } else {
+            this.lat = locationIntent.getStringExtra(getResources().getString(R.string.latitude));
+            this.lon = locationIntent.getStringExtra(getResources().getString(R.string.longitude));
+            myClickHandler(false);
+        }
+
 
 //        try {
 //            parseURL("congress.api.sunlightfoundation.com/legislators/locate?latitude=37&longitude=-122&apikey=391dab8747c74ab2bc6bffc5357d81bd");
@@ -102,6 +123,86 @@ public class CongressionalViewActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    // Before attempting to fetch the URL, makes sure that there is a network connection.
+    public void myClickHandler(boolean isZip) {
+        // Gets the URL from the UI's text field.
+        //String stringUrl = "http://www.espn.com";
+        String stringUrl;
+        if (isZip) {
+            stringUrl = "https://congress.api.sunlightfoundation.com/legislators/locate?"
+                    + "zip=" + this.zipCode
+                    + "&apikey=" + getResources().getString(R.string.sunlight_API_key);
+        } else {
+            stringUrl = "https://congress.api.sunlightfoundation.com/legislators/locate?"
+                    + "latitude=" + this.lat
+                    + "&longitude=" + this.lon
+                    + "&apikey=" + getResources().getString(R.string.sunlight_API_key);
+        }
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            Log.d(DEBUG_TAG, "No network connection available.");
+        }
+    }
+
+    // Uses AsyncTask to create a task away from the main UI thread. This task takes a
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string, which is
+    // displayed in the UI by the AsyncTask's onPostExecute method.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            //textView.setText(result);
+            Log.d(DEBUG_TAG, result);
+        }
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+// the web page content as a InputStream, which it returns as
+// a string.
+    private String downloadUrl(String myurl) throws IOException {
+        //String email = emailText.getText().toString();
+        // Do some validation here
+
+        try {
+            URL url = new URL(myurl);
+            //URL url = new URL(API_URL + "email=" + email + "&apiKey=" + API_KEY);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
     }
 
 
